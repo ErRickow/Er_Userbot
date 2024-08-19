7851a66f961ef19aa6a1a50d234ae064e884f0bb
Just R <remixingmagelang@gmail.com>
2024-08-20 02:59:30 +0700

    update plugins/devtools.py

--- plugins/devtools.py
+++ plugins/devtools.py
@@ -312,7 +312,7 @@ async def _(event):
                 reply_to=reply_to_id,
             )
         return await xx.delete()
-    await xx.reply(final_output)
+    await xx.reply(final_output, parse_mode='html')
 
 
 def _stringify(text=None, *args, **kwargs):
