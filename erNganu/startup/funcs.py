70634036d9a3ef09029a27251aa7f1d7f44a111a
Just R <remixingmagelang@gmail.com>
2024-08-20 02:39:17 +0700

    update erNganu/startup/funcs.py

--- erNganu/startup/funcs.py
+++ erNganu/startup/funcs.py
@@ -383,7 +383,7 @@ async def customize():
         await asyncio.sleep(1)
         await eruser_bot.send_message(
             "botfather",
-            f"Er Userbot Assistant\nNganunya ~ {sir} \n\nSi Perusak @chakszzz,
+            f"Er Userbot Assistant\nNganunya ~ {sir} \n\nSi Perusak @chakszzz",
         )
         await asyncio.sleep(2)
         await msg.edit("Selesai **Auto Costumisasi** di @BotFather.")
