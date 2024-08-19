8140abe8c379c240b9c23ab6245f85144cc5c3f1
Just R <remixingmagelang@gmail.com>
2024-08-20 05:49:31 +0700

    update plugins/updater.py

--- plugins/updater.py
+++ plugins/updater.py
@@ -1,3 +1,63 @@
+from . import get_help
+
+__doc__ = get_help("help_updater")
+
+import os
+import sys
+import time
+from platform import python_version as pyver
+from random import choice
+
+from telethon import __version__
+from telethon.errors.rpcerrorlist import (
+    BotMethodInvalidError,
+    ChatSendMediaForbiddenError,
+)
+
+from erNganu.version import __version__ as ErUbotVer
+
+from . import HOSTED_ON, LOGS
+
+try:
+    from git import Repo
+except ImportError:
+    LOGS.error("bot: 'gitpython' module not found!")
+    Repo = None
+
+from telethon.utils import resolve_bot_file_id
+
+from . import (
+    ATRA_COL,
+    LOGS,
+    OWNER_NAME,
+    ULTROID_IMAGES,
+    Button,
+    Carbon,
+    Telegraph,
+    Var,
+    allcmds,
+    asst,
+    bash,
+    call_back,
+    callback,
+    def_logs,
+    eor,
+    get_string,
+    heroku_logs,
+    in_pattern,
+    inline_pic,
+    restart,
+    shutdown,
+    start_time,
+    time_formatter,
+    udB,
+    erubot_cmd,
+    eruserbot_version,
+    updater,
+)
+
+
+
 @erubot_cmd(pattern="up( (.*)|$)")
 async def _(e):
     xx = await e.reply(get_string("upd_1"))
