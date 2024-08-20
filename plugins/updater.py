2b0ee36094a28426e90eabfe2e5ece327d5d44d3
Just R <remixingmagelang@gmail.com>
2024-08-20 07:11:18 +0700

    update plugins/updater.py

--- plugins/updater.py
+++ plugins/updater.py
@@ -1,79 +1,38 @@
-from . import get_help
-
-__doc__ = get_help("help_updater")
-
-import os
-import sys
-import time
-from platform import python_version as pyver
-from random import choice
-
-from telethon import __version__
-from telethon.errors.rpcerrorlist import (
-    BotMethodInvalidError,
-    ChatSendMediaForbiddenError,
-)
-
-from erNganu.version import __version__ as ErUbotVer
-
-from . import HOSTED_ON, LOGS
-
-try:
-    from git import Repo
-except ImportError:
-    LOGS.error("bot: 'gitpython' module not found!")
-    Repo = None
-
-from telethon.utils import resolve_bot_file_id
-
-from . import (
-    ATRA_COL,
-    LOGS,
-    OWNER_NAME,
-    ULTROID_IMAGES,
-    Button,
-    Carbon,
-    Telegraph,
-    Var,
-    allcmds,
-    asst,
-    bash,
-    call_back,
-    callback,
-    def_logs,
-    eor,
-    get_string,
-    heroku_logs,
-    in_pattern,
-    inline_pic,
-    restart,
-    shutdown,
-    start_time,
-    time_formatter,
-    udB,
-    erubot_cmd,
-    eruserbot_version,
-    updater,
-)
-
-
+##kurang lebih gini:
 
 @erubot_cmd(pattern="up( (.*)|$)")
-async def _(e):
-    xx = await e.reply(get_string("upd_1"))
-    if e.pattern_match.group(1).strip() and (
-        "fast" in e.pattern_match.group(1).strip()
-        or "soft" in e.pattern_match.group(1).strip()
-    ):
-        await bash("git pull -f && pip3 install -r requirements.txt")
-        call_back()
-        await xx.edit(get_string("upd_7"))
-        os.execl(sys.executable, "python3", "-m", "erNganu")
-        # return
-
-    else:
-        await xx.edit(
-            f'<blockquote><code>Your BOT is </code><strong>up-to-date</strong><code> with </code><strong>nganu</a></strong></blockquote>',
-            parse_mode="html",
-            link_preview=False,
+async def ngapdate(client, message):
+    emo = Emo(client.me.id)
+    await emo.initialize()
+    pros = await message.reply(
+        f"<blockquote>{emo.proses} <b>Memeriksa pembaruan resources {bot.me.mention} ..</b></blockquote>"
+    )
+    out = subprocess.check_output(["git", "pull"]).decode("UTF-8")
+    teks = f"<b>❒ Status resources {bot.me.mention}:</b>\n"
+    memeg = f"{emo.profil} <b>Change logs {bot.me.mention}</b>"
+    if "Already up to date." in str(out):
+        return await pros.edit(f"<blockquote>{teks}┖ {out}</blockquote>")
+    elif len(out) > 4096:
+        anuk = await pros.edit(
+            f"<blockquote>{emo.proses} <b>Hasil akan dikirimkan dalam bentuk file ..</b></blockquote>"
         )
+        with open("output.txt", "w+") as file:
+            file.write(out)
+
+        X = f"<blockquote>{emo.alive} <b>Change logs {bot.me.mention}</b></blockquote>"
+        await client.send_document(
+            message.chat.id,
+            "output.txt",
+            caption=f"{X}",
+            reply_to_message_id=message.id,
+        )
+        await anuk.delete()
+        os.remove("output.txt")
+    else:
+        format_line = [f"┣ {line}" for line in out.splitlines()]
+        if format_line:
+            format_line[-1] = f"┖ {format_line[-1][2:]}"
+        format_output = "\n".join(format_line)
+
+        await pros.edit(f"<blockquote>{memeg}\n\n{teks}{format_output}</blockquote>")
+    os.execl(sys.executable, sys.executable, "-m", "ubot")
