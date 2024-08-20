b62a9ad2f511f71dde1a8c1519d8b92486d682c3
Just R <remixingmagelang@gmail.com>
2024-08-20 18:09:29 +0700

    update plugins/updater.py

--- plugins/updater.py
+++ plugins/updater.py
@@ -43,6 +43,28 @@ async def _(a):
         "fast" in a.pattern_match.group(1).strip()
         or "soft" in a.pattern_match.group(1).strip()
     ):
+      await bash("git pull -f && pip3 install -r requirements.txt")
+        call_back()
+        await xx.edit(get_string("upd_7"))
+        os.execl(sys.executable, "python3", "-m", "erNganu")
+        # return
+    m = await updater()
+    branch = (Repo.init()).active_branch
+    if m:
+        x = await asst.send_file(
+            udB.get_key("LOG_CHANNEL"),
+            ULTPIC(),
+            caption="• **Tersedia Update** •",
+            force_document=False,
+            buttons=Button.inline("Changelogs", data="changes"),
+        )
+        Link = x.message_link
+        await xx.edit(
+            f'<strong><a href="{Link}">[ChangeLogs]</a></strong>',
+            parse_mode="html",
+            link_preview=False,
+        )
+    else:
         await xx.reply(
             f'<blockquote><code>Your BOT is </code><strong>up-to-date</strong><code> with </code><strong>nganu</a></strong></blockquote>',
             parse_mode="html",
