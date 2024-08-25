7969f9dbe7a3eee8aedccc6b1ce7af8a5ca3c675
Just R <remixingmagelang@gmail.com>
2024-08-26 01:07:44 +0700

    update plugins/update.py

--- plugins/update.py
+++ plugins/update.py
@@ -70,13 +70,14 @@ async def _(e):
         with open("output.txt", "w+") as file: 
           file.write(out)
 
-#os.remove("output.txt")
+          os.remove("output.txt")
           format_line = [f"┣ {line}" for line in out.splitlines()]
-if format_line:
-  format_line[-1] = f"┖ {format_line[-1][2:]}"
-  format_output = "\n".join(format_line)
-pros.edit(f"<blockquote>{memeg}\n\n{teks}{format_output}</blockquote>")
-os.execl(sys.executable, sys.executable, "python3", "-m", "erNganu")
+          if format_line:
+            
+            format_line[-1] = f"┖ {format_line[-1][2:]}"
+            format_output = "\n".join(format_line)
+            pros.edit(f"<blockquote>{memeg}\n\n{teks}{format_output}</blockquote>")
+            os.execl(sys.executable, sys.executable, "python3", "-m", "erNganu")
   
 @callback("updtavail", owner=True)
 async def updava(event):
