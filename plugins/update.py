52716b55d015c070fe3657939a284f72daf57613
Just R <remixingmagelang@gmail.com>
2024-08-26 00:52:36 +0700

    update plugins/update.py

--- plugins/update.py
+++ plugins/update.py
@@ -67,7 +67,8 @@ async def _(e):
           anuk = await pros.edit(
             f"<blockquote> <b>Hasil akan dikirimkan dalam bentuk file ..</b></blockquote>"
         )
-        with open("output.txt", "w+") as file: file.write(out)
+        with open("output.txt", "w+") as file: 
+          file.write(out)
 
 os.remove("output.txt")
   format_line = [f"┣ {line}" for line in out.splitlines()]
