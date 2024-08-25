02ba3a69989e1179de8b67cb5b6a0c1931d73743
Just R <remixingmagelang@gmail.com>
2024-08-26 00:56:41 +0700

    update plugins/update.py

--- plugins/update.py
+++ plugins/update.py
@@ -75,7 +75,7 @@ format_line = [f"┣ {line}" for line in out.splitlines()]
 if format_line:
   format_line[-1] = f"┖ {format_line[-1][2:]}"
   format_output = "\n".join(format_line)
-  await pros.edit(f"<blockquote>{memeg}\n\n{teks}{format_output}</blockquote>")
+await pros.edit(f"<blockquote>{memeg}\n\n{teks}{format_output}</blockquote>")
   os.execl(sys.executable, sys.executable, "python3", "-m", "erNganu")
   
 @callback("updtavail", owner=True)
