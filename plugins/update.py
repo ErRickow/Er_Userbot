c1ee2d6041e07dd69062958c4ab8a2bc8d4fb6fa
Just R <remixingmagelang@gmail.com>
2024-08-26 00:50:11 +0700

    update plugins/update.py

--- plugins/update.py
+++ plugins/update.py
@@ -67,7 +67,7 @@ if len(out) > 4096:
           anuk = await pros.edit(
             f"<blockquote> <b>Hasil akan dikirimkan dalam bentuk file ..</b></blockquote>"
         )
-  with open("output.txt", "w+") as file:
+        with open("output.txt", "w+") as file:
             file.write(out)
 
 os.remove("output.txt")
